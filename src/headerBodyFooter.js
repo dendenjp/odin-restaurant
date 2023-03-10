2daa2f40b6b4268201c6a2198357c39ededa5e0a
dendenjp <daimlerjp@gmail.com>
2023-03-10 00:09:35 -0600

    finished header

---
+++ src/headerBodyFooter.js
@@ -0,0 +1,60 @@
+function backgroundImage() {
+	const body = document.querySelector('body');
+	body.style.backgroundImage = 'url("../img/background-image Cropped.jpg")';
+	body.style.backgroundSize = 'cover';
+	body.style.backgroundRepeat = 'no-repeat';
+	body.style.backgroundAttachment = 'fixed';
+
+	const overlay = document.createElement('div');
+	overlay.style.position = 'absolute';
+	overlay.style.top = '0';
+	overlay.style.left = '0';
+	overlay.style.width = '100%';
+	overlay.style.height = '100%';
+	overlay.style.backgroundColor = 'rgba(0,0,0,0.3)'; //rgba(0,0,0,0.3)
+	overlay.style.zIndex = '-1';
+	body.appendChild(overlay);
+}
+
+function createHeader() {
+	const header = document.createElement('header');
+	header.classList.add('header');
+
+	const restaurantName = document.createElement('h1');
+	restaurantName.classList.add('restaurant-name');
+	restaurantName.textContent = 'STARBUKO';
+
+	header.appendChild(restaurantName);
+	header.appendChild(createNavbar());
+	return header;
+}
+
+function createNavbar() {
+	const nav = document.createElement('nav');
+	nav.classList.add('navbar');
+
+	const ul = document.createElement('ul');
+	ul.classList.add('navbar-list');
+
+	const homeLi = document.createElement('li');
+	homeLi.classList.add('navbar-item', 'home', 'active');
+
+	const menuLi = document.createElement('li');
+	menuLi.classList.add('navbar-item', 'menu');
+
+	const contactLi = document.createElement('li');
+	contactLi.classList.add('navbar-item', 'contact');
+
+	homeLi.textContent = 'Home';
+	menuLi.textContent = 'Menu';
+	contactLi.textContent = 'Contact Us!';
+
+	ul.appendChild(homeLi);
+	ul.appendChild(menuLi);
+	ul.appendChild(contactLi);
+
+	nav.appendChild(ul);
+	return nav;
+}
+
+export { createHeader, createNavbar, backgroundImage };
